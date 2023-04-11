3d487866c19a6e02ec8bfb8b4a5d1572d77ab62d
TommysNJ <TommysNJ@users.noreply.github.com>
2023-04-10 21:55:00 -0500

    Constructor Proceso
    Adición del constructor de la clase proceso.

--- src/Proceso.java
+++ src/Proceso.java
@@ -2,4 +2,10 @@ public class Proceso {
     private String id;
     private long cedula;
     private int tiempo;
+
+    public Proceso(String id, long cedula, int tiempo) {
+        this.id = id;
+        this.cedula = cedula;
+        this.tiempo = tiempo;
+    }
 }
